64e2a9961a2e78056cc33b1ea92c41b08b288c5a
samnkrki <samn_krki@live.com>
2020-04-02 09:37:04 +0545

    added more pandas functions

--- pandas.py
+++ pandas.py
@@ -53,3 +53,26 @@ df_grouped = df.groupby(["salary"], as_index = False)["satisfaction_level"].sum(
 df_grouped = df.groupby(["salary"], as_index = False)["satisfaction_level"].mean()
 #multiple groupby
 df_grouped_multiple = df.groupby(["salary","sales"], as_index = False)["satisfaction_level"].sum()
+#merge- happens on a specfied column, join-happens on a key(index)
+left = df
+right = df.groupby(['sales'], as_index = False)['satisfaction_level'].mean()
+right.columns = ["sales","satisfaction_index"]
+df_merged = pd.merge(left, right, how = "inner", on = ["sales"])
+
+#join
+left1 = df.iloc[:1500,:4]
+right1 = df.iloc[:500,7:]
+
+joined = left1.join(right1, how = "inner")
+joined = left1.join(right1, how = "left")
+joined = left1.join(right1, how = "right")
+
+#concat
+df_grouped1 = df.groupby(['sales'], as_index = False)['satisfaction_level'].mean()
+df_grouped2 = df.groupby(['sales'], as_index = False)['satisfaction_level'].sum()
+concated = pd.concat([df_grouped1, df_grouped2])#vertical concat, adds two data in one
+concated = pd.concat([df_grouped1, df_grouped2], axis = 1)
+
+#pivot
+df_pvt =  df.groupby(['sales', 'salary'], as_index = False)['satisfaction_level'].mean()
+df_pvt2 = df_pvt.pivot(index = "sales", columns = "salary", values = "satisfaction_level")
